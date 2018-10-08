62e87a8732df86efb242ae2c6950bc7c46d0f927
NeonSpectrum <youngskymann@gmail.com>
2018-10-08 09:21:13 +0800

    added logo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,8 +3,13 @@
 <div class="row">
   <div id="admin" class="col s12">
     <div class="card material-table">
+      <div class="center-align" style="padding-top:10px;width:100%">
+        <img src="{{ asset('public/img/rnd.png') }}" alt="" height="80px">
+        <img src="{{ asset('public/img/ue_thumb.png') }}" alt="" height="80px">
+        <img src="{{ asset('public/img/ccss_thumb.png') }}" alt="" height="80px">
+      </div>
       <div class="table-header">
-        <span class="table-title">MOA Signing System</span>
+        <span class="table-title">College Partnership Archiving System</span>
         <div class="actions">
           <a href="#" class="btnUpload modal-trigger waves-effect btn-flat nopadding"><i class="material-icons">file_upload</i></a>
           <a href="#addModal" class="modal-trigger waves-effect btn-flat nopadding"><i class="material-icons">add</i></a>
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -6,6 +6,6 @@
   <link rel="stylesheet" href="{{ asset('public/css/materialize.min.css') }}">
   <link rel="stylesheet" href="{{ asset('public/css/style.css') }}">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <title>MOA Signing System</title>
+  <title>College Partnership Archiving System</title>
 </head>
 <body>
